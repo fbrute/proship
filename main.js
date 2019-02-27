6b587361307afbc8fb30f5abf678c156070b2099
France-Nor Brute <fbrute@gmail.com>
2019-02-27 19:04:54 +0100

    extract sql building from main.js to sql.js

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 'use strict';
 
-// var contents = fs.readFileSync("modtfregressions_Guadeloup_Ocean_2012_50.json");
-// var regressions = JSON.parse(contents);
-
-
 const promise = require('bluebird');
 const initOptions = {
   promiseLib: promise, // overriding the default (ES6 Promise);
 };
 
-
-
 const pgp = require('pg-promise')(initOptions);
-// See also: http://vitaly-t.github.io/pg-promise/module-pg-promise.html
 
 // Database connection details;
 const cn = {
@@ -45,92 +38,29 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-app.get('/api/v1/shipments_working', function(req, response, next) {
-  db.task(t => {
-    // execute a chain of queries against the task context, and return the result:
-    // return t.one('SELECT count(*) FROM shipments WHERE company_id = $1', 2, a => +a.count)
-    return t.none('select sp.id as sid, quantity, p.id as id, sku, description into temp table sp2' +
-        'from shipment_products sp inner join products p on sp.id = p.id', a => +a.count)
-      .then(count => {
-        if (count > 0) {
-          return t.any('SELECT * FROM products')
-            .then(logs => {
-              return {count, logs};
-            });
-        }
-        return {count};
-      });
-  })
-    .then(data => {
-      // success, data = either {count} or {count, logs}
-      console.log(data);
-      response.status(200).json({ records: data });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
 app.get('/api/v1/shipments', function(req, response, next) {
   // parsing the request parameters
-  var query = require('url').parse(req.url, true).query;
-  var company_id = query.company_id;
+  var query_parameters = require('url').parse(req.url, true).query;
 
-  if (typeof company_id == 'undefined' || Number(company_id) <= 0) {
-    response.status(422).json({ errors: ['company_id is required'] });
-  }
+  // get the query string to be performed
+  var sql_query = require('./sql.js').shipment_build_sql(query_parameters);
 
-  var transport_mode = query.international_transportation_mode;
-  var transport_mode_sql_filter
-  if (typeof transport_mode == 'undefined') {
-    transport_mode_sql_filter = '';
-  } else if ( transport_mode !== 'ocean' && transport_mode !== 'truck' ){
-    response.status(422).json({ errors: ['international transportation mode filter should be ocean or truck'] });
-  }
-  else {
-      transport_mode_sql_filter = ' and international_transportation_mode = ' + "'" +  transport_mode + "' " ;
-      console.log(transport_mode_sql_filter);
-  }
-  
-  // Handle pagination
-  var page = query.page || 1;
-  var per = query.per || 4;
-  var pagination_sql = ' LIMIT ' + per + ' OFFSET (' + page + ' - 1) * ' + per + ' '
-  console.log(pagination_sql);
 
-  console.log(Number(company_id));
+  if (sql_query.errors['errors'].length > 0) {
+    response.status(422).json(sql_query.errors);
+  } else {
 
-  
-  // default direction of sorting by international_departure_date is (asc)ending
-  var sort = query.sort || 'international_departure_date';
-  if (sort.toUpperCase() !== 'INTERNATIONAL_DEPARTURE_DATE') {
-    response.status(422).json({ errors: ['sorting field should be international_departure_date'] });
-  }
-
-  var direction = query.direction || 'asc';
-  const directionUp = direction.toUpperCase();
-
-  // Check sorting direction value
-  if (directionUp !== 'ASC' && directionUp !== 'DESC') {
-    response.status(422).json({ errors: ['sorting direction should be ASC or DESC'] });
-  }
-
-  console.log('company_id=' + company_id);
-  console.log('direction=' + direction);
-
-  
-  db.any("SELECT s.id as id, s.name as name ,json_agg(to_jsonb(sp) - 'sid') as products FROM shipments s, (select sp.shipment_id as sid, quantity, p.id as id, sku, description, row_number () over ( partition by sp.shipment_id) active_shipment_count from shipment_products sp, products p where sp.product_id = p.id) sp WHERE company_id = $1" + transport_mode_sql_filter + ' and sp.sid = s.id GROUP BY s.id order by ' + sort + ' ' + direction + pagination_sql , company_id)
-    .then(data => {
-      console.log(data);
-      response.status(200).json({ records: data });
-    })
-    .catch(error => {
+    db.any(sql_query.string)
+      .then(data => {
+        console.log(data);
+        response.status(200).json({ records: data });
+      })
+      .catch(error => {
       // failed
-      console.log(error);
-    });
+        console.log(error);
+      });
+  }
 });
-
 
 app.listen(port, hostname, function() {
   console.log(`Server running at http://${hostname}:${port}/`);
--- sql.js
+++ sql.js
@@ -1,17 +1,51 @@
 // File sql.js
 // Organize an sql provider
 //
-const QueryFile = require('pg-promise').QueryFile;
-const path = require('sql');
+'use strict';
 
-// Helper for linking to external query files:
-function sql(file) {
-    const fullPath = path.join(__dirname, file); // generating full path;
-    return new QueryFile(fullPath, {minify: true});
-}
+exports.shipment_build_sql = function(query) {
+  // parsing the request parameters
+  var company_id = query.company_id;
+  // object to be receiving the results of the sql string building or errors
+  var sql_query = { string: '', errors: { errors: []} };
 
-module.exports = {
-    // external queries for Users:
-    shipments: sql('shipments.sql'),
+  if (typeof company_id === 'undefined' || Number(company_id) <= 0) {
+    sql_query.errors = { errors: ['company_id is required'] };
+    return (sql_query);
+  }
+
+  var transport_mode = query.international_transportation_mode;
+  var transport_mode_sql_filter;
+  if (typeof transport_mode === 'undefined') {
+    transport_mode_sql_filter = '';
+  } else if (transport_mode !== 'ocean' && transport_mode !== 'truck'){
+    sql_query.errors = { errors: ['international transportation mode filter should be ocean or truck'] };
+    return sql_query;
+  } else {
+    transport_mode_sql_filter = ' and international_transportation_mode = ' + "'" + transport_mode + "' ";
+  }
+  // default direction of sorting by international_departure_date is (asc)ending
+  var sort = query.sort || 'international_departure_date';
+  if (sort.toUpperCase() !== 'INTERNATIONAL_DEPARTURE_DATE') {
+    sql_query.errors = { errors: ['sorting field should be international_departure_date'] };
+    return sql_query;
+  }
+
+  var direction = query.direction || 'asc';
+  const directionUp = direction.toUpperCase();
+
+  // Check sorting direction value
+  if (directionUp !== 'ASC' && directionUp !== 'DESC') {
+    sql_query.errors = { errors: ['sorting direction should be ASC or DESC'] };
+    return sql_query;
+  }
+
+  // Handle pagination
+  var page = query.page || 1;
+  var per = query.per || 4;
+  var pagination_sql = ' LIMIT ' + per + ' OFFSET (' + page + ' - 1) * ' + per + ' ';
+
+  sql_query.string = `SELECT s.id as id, s.name as name ,json_agg(to_jsonb(sp) - 'sid') as products FROM shipments s, (select sp.shipment_id as sid, quantity, p.id as id, sku, description, row_number () over ( partition by sp.shipment_id) active_shipment_count from shipment_products sp, products p where sp.product_id = p.id) sp WHERE company_id = ${company_id} ${transport_mode_sql_filter} and sp.sid = s.id GROUP BY s.id order by ${sort} ${direction} ${pagination_sql}`;
+
+  return sql_query;
 };
-
