d59a19605e2b54c9fd2a15a09f1eafe93ec1d4d7
France-Nor Brute <fbrute@gmail.com>
2019-02-28 02:23:43 +0100

    reduce smells

--- sql.js
+++ sql.js
@@ -15,7 +15,7 @@ exports.shipment_build_sql = function(request) {
     }
   };
 
-  function validParams() {
+  function validParamsCompanyId() {
     //parsing the request parameters
     const company_id = request.company_id;
     if (typeof company_id === 'undefined' || Number(company_id) <= 0) {
@@ -24,6 +24,10 @@ exports.shipment_build_sql = function(request) {
     else {
       sql_query.params.company_id = company_id;
     }
+  };
+
+
+  function validParamsTransportFilter() {
     // filter by international transportation mode
     const transport_mode = request.international_transportation_mode;
     if (typeof transport_mode === 'undefined' && transport_mode !== 'ocean' && transport_mode !== 'truck') {
@@ -31,7 +35,9 @@ exports.shipment_build_sql = function(request) {
     } else {
       sql_query.params.transport_mode_sql_filter = ' and international_transportation_mode = ' + "'" + transport_mode + "' ";
     }
+  };
 
+  function validParamsSortingDirection() {
     // default direction of sorting by international_departure_date is (asc)ending
     const sort = request.sort || 'international_departure_date';
     if (sort.toUpperCase() !== 'INTERNATIONAL_DEPARTURE_DATE') {
@@ -48,7 +54,9 @@ exports.shipment_build_sql = function(request) {
     } else {
       sql_query.params.direction = direction;
     }
+  };
 
+  function validParamsPagination() {
     // Handle pagination
     const page = request.page || 1;
     const per = request.per || 4;
@@ -59,7 +67,10 @@ exports.shipment_build_sql = function(request) {
     return `SELECT s.id as id, s.name as name ,json_agg(to_jsonb(sp) - 'sid') as products FROM shipments s, (select sp.shipment_id as sid, quantity, p.id as id, sku, description, row_number () over ( partition by sp.shipment_id) active_shipment_count from shipment_products sp, products p where sp.product_id = p.id) sp WHERE company_id = ${sql_query.params.company_id} ${sql_query.params.transport_mode_sql_filter} and sp.sid = s.id GROUP BY s.id order by ${sql_query.params.sort} ${sql_query.params.direction} ${sql_query.params.pagination_sql}`;
   };
 
-  validParams();
+  validParamsCompanyId();
+  validParamsTransportFilter();
+  validParamsSortingDirection();
+  validParamsPagination();
 
   console.log('length:' + sql_query.errors['errors'].length ); 
 
